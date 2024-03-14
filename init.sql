1a0f752484e62812d905827c4ef64bdf9488ba9d
Even Torbjørnsen <591118@stud.hvl.no>
2024-03-14 12:48:21 +0100

    database-skjema

--- init.sql
+++ init.sql
@@ -1,16 +1,16 @@
 -- Create the frmc database
-CREATE DATABASE frmc;
+CREATE DATABASE frmc IF NOT EXISTS frmc;
 
 -- Connect to the frmc database
-\c frmc;
+\c frcm;
 
--- Create the frmc table
-CREATE TABLE frmc (
-    Lokasjon VARCHAR(50) PRIMARY KEY,
-    Dato INT,
-    temp DOUBLE PRECISION,
-    fuktighet DOUBLE PRECISION,
-    vind INT,
-    regnfall DOUBLE PRECISION,
-    Firerisk DOUBLE PRECISION
+CREATE TABLE IF NOT EXISTS frcm (
+    id INTEGER PRIMARY KEY,
+    lokasjon TEXT,
+    dato TEXT,
+    temp REAL,
+    fuktighet REAL,
+    vind REAL,
+    regnfall REAL,
+    firerisk REAL
 );
