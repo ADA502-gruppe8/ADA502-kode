afb26b4d6e5968c31d157b1b85beb5352c0e1e94
Yosafe <581515@stud.hvl.no>
2024-03-21 13:50:55 +0100

    extractor first try, updated Obaservations Model and Forcasts Model

--- src/frcm/api/data_base_models.py
+++ src/frcm/api/data_base_models.py
@@ -3,9 +3,19 @@ import datetime
 from frcm.datamodel.model import Location, WeatherDataPoint, Observations, Forecast, WeatherData, FireRisk, \
     FireRiskPrediction
 
+from frcm.fireriskmodel.compute import compute
+
 location = Location(latitude=51.509865, longitude=-0.118092)
-weather_data_point = WeatherDataPoint(
+weather_data_point1 = WeatherDataPoint(
     timestamp=datetime.datetime.now(),
+    temperature=25.0,
+    humidity=0.5,
+    precipitation=0.0,
+    wind_speed=5.0,
+    wind_direction=180.0
+)
+weather_data_point2 = WeatherDataPoint(
+    timestamp=datetime.datetime.now()+datetime.timedelta(hours=1),
     temperature=25.0,
     humidity=0.5,
     precipitation=0.0,
@@ -16,12 +26,12 @@ weather_data_point = WeatherDataPoint(
 observations = Observations(
     source='met_office',
     location=location,
-    data=[weather_data_point]
+    data=[weather_data_point1]
 )
 
 forecast = Forecast(
     location=location,
-    data=[weather_data_point]
+    data=[weather_data_point2]
 )
 
 weather_data = WeatherData(
@@ -40,6 +50,9 @@ fire_risk_prediction = FireRiskPrediction(
     firerisks=[fire_risk]
 )
 
-print(weather_data)
-print(fire_risk_prediction)
+#print(weather_data)
+#print(fire_risk_prediction)
 
+
+fire_risk_prediction = compute(weather_data)
+print(fire_risk_prediction)
