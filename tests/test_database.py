27bf4ee93135638bf9c259a0e4de7fd6ba874bd0
581515 <47955690+Y581515@users.noreply.github.com>
2024-03-31 17:15:14 +0200

    postgres_host

--- tests/test_database.py
+++ tests/test_database.py
@@ -4,7 +4,7 @@ import unittest
 class TestDatabaseConnection(unittest.TestCase):
     def test_database(self):
         conn_params = {
-            'host': 'po',
+            'host': 'postgres_host',
             'database': 'postgres',
             'port':  5432,
             'user': 'postgres',
