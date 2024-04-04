a3b18a6bb1f92024d481b7452cff1b729ef45062
Even Torbjørnsen <591118@stud.hvl.no>
2024-04-04 13:43:00 +0100

    Merge specific files from docker branch into main

--- init.sql
+++ init.sql
@@ -1,16 +1,18 @@
--- Create the frmc database
-CREATE DATABASE frmc;
+-- Create the frmc database if it does not exist
+CREATE DATABASE IF NOT EXISTS frmc;
 
 -- Connect to the frmc database
 \c frmc;
 
--- Create the frmc table
-CREATE TABLE frmc (
-    Lokasjon VARCHAR(50) PRIMARY KEY,
-    Dato INT,
-    temp DOUBLE PRECISION,
-    fuktighet DOUBLE PRECISION,
-    vind INT,
-    regnfall DOUBLE PRECISION,
-    Firerisk DOUBLE PRECISION
+-- Create the frcm table with columns defined with TIMESTAMP datatype for "dato" column and "tid" column
+CREATE TABLE IF NOT EXISTS frcm (
+    id SERIAL PRIMARY KEY,
+    lokasjon TEXT,
+    dato DATE,
+    tid TIMESTAMP,
+    temp NUMERIC(4, 2),
+    fuktighet NUMERIC(4, 2),
+    vind NUMERIC(4, 2),
+    regnfall NUMERIC(4, 2),
+    firerisk NUMERIC(4, 2)
 );
